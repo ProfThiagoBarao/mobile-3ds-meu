0a1955cc23138191f30f1324cd09fa6a7552769f
ProfThiagoBarao <thiago.forteski@escola.pr.gov.br>
2025-03-18 00:43:33 -0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App</Text>
+      <Text>Open teste</Text>
       <StatusBar style="auto" />
     </View>
   );
